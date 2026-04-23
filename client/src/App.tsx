82dbb15e55c64164515fed1f466d8d6339ae42d4
shunadesu <namp280918@gmail.com>
2026-04-23 16:41:40 +0700

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,10 +11,10 @@ import { OverviewSection2 } from './components/sections/OverviewSection2';
 const SITE_CONFIG = {
   name: 'Biệt Thự Sola Đảo Ảnh Dương',
   shortName: 'Sola Global City',
-  description: 'Dự án biệt thự cao cấp, shophouse tại vị trí đắc địa Quận 2, TP. Hồ Chí Minh. Giá biệt thự từ 8.5 tỷ, shophouse từ 5.2 tỷ.',
+  description: 'Dự án biệt thự cao cấp, SOLA VILLAS THE GLOBAL CITY BIỂU TƯỢNG VILLA COMPOUND DUY NHẤT TẠI GLOBAL CITY Phân khu Sola - Đảo Ánh Dương nằm trong khu đô thị The Global City',
   url: 'https://soladaoanhduong.nthanhproperty.fun',
   ogImage: 'https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?w=1200&q=80',
-  keywords: 'biệt thự sola đảo ảnh dương, biệt thự sola global city, sola dao anh duong, biệt thự quận 2, shophouse quận 2, bất động sản quận 2',
+  keywords: 'biệt thự sola đảo ảnh dương, Biệt thự Sola, biệt thự sola global city, sola dao anh duong, biệt thự quận 2, Biệt thự Sola The Global City - Đảo Ánh Dương, bất động sản quận 2',
 };
 
 function PageTracker() {
@@ -37,7 +37,7 @@ function Home() {
   return (
     <>
       <Helmet>
-        <title>Biệt Thự Sola Đảo Ảnh Dương | Biệt Thự Sola Global City Quận 2 Giá Tốt</title>
+        <title>Biệt Thự Sola Đảo Ảnh Dương | Biệt Thự Sola Global City Quận 2</title>
         <meta name="description" content={SITE_CONFIG.description} />
         <meta name="keywords" content={SITE_CONFIG.keywords} />
         <meta property="og:title" content="Biệt Thự Sola Đảo Ảnh Dương | Biệt Thự Sola Global City Quận 2" />
